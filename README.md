5a5ca3fa087e074ca20de549b9ca09a38cf62c8c
xLsDg <xlsdg@qq.com>
2016-09-20 21:13:52 +0800

    1.初始化源代码；

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# gulp-together
-Streaming concat package for gulp
+# gulp-together [![Build Status](https://travis-ci.org/xlsdg/gulp-together.svg?branch=master)](https://travis-ci.org/xlsdg/gulp-together)
+
+> My majestic gulp plugin
+
+
+## Install
+
+```
+$ npm install --save-dev gulp-together
+```
+
+
+## Usage
+
+```js
+const gulp = require('gulp');
+const together = require('gulp-together');
+
+gulp.task('default', () => {
+	gulp.src('src/file.ext')
+		.pipe(together())
+		.pipe(gulp.dest('dist'))
+);
+```
+
+
+## API
+
+### together([options])
+
+#### options
+
+##### foo
+
+Type: `boolean`<br>
+Default: `false`
+
+Lorem ipsum.
+
+
+## License
+
+MIT © [xLsDg](https://github.com/xlsdg/gulp-together)
---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var gutil = require('gulp-util');
+var through = require('through2');
+var PluginError = gutil.PluginError;
+var File = gutil.File;
+
+module.exports = function (fileArray, opts) {
+    if (!fileArray) {
+        throw new PluginError('gulp-together', 'Missing fileArray option for gulp-together');
+    }
+
+    if (!(fileArray instanceof Array)) {
+        throw new PluginError('gulp-together', 'Error fileArray option for gulp-together');
+    }
+
+    opts = opts || {};
+
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            cb(null, file);
+            return;
+        }
+
+        if (file.isStream()) {
+            cb(new PluginError('gulp-together', 'Streaming not supported'));
+            return;
+        }
+
+        try {
+            var cwd = process.cwd();
+            var contents = [];
+
+            for (var i = 0, len = fileArray.length, modPath, modFile; i < len; i++) {
+                modPath = cwd + '/node_modules/' + fileArray[i];
+                modFile = modPath + '/' + require(modPath + '/package.json').main;
+                contents.push(String(fs.readFileSync(modFile, 'utf8')));
+            }
+
+            file.contents = new Buffer(contents.join('') + file.contents.toString());
+            this.push(file);
+        } catch (err) {
+            this.emit('error', new PluginError('gulp-together', err));
+        }
+
+        cb();
+    });
+};
